0e2aceb14f165fbeea31d0efe05dea57d86c6a31
lteruki <31689556+lteruki@users.noreply.github.com>
2018-01-16 11:36:48 +0300

    Update abc.java

--- Lab3/abc.java
+++ Lab3/abc.java
@@ -1,11 +1,11 @@
 package Lab3;
 
 public class abc {
-	public static double kat(double c, double a){
+	public static double abc(double c, double a){
 		double f= Math.sqrt(c*c-a*a);
 		return f;
 		}
 		public static void main(String[] args) {
-		System.out.println (kat(9,3));
+		System.out.println (abc(9,3));
 		}
 }
